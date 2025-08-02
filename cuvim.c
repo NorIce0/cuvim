4fba3b1e860811bab8ddee0740816edd5fa904e0
Esteban <euadenez8@gmail.com>
2025-08-02 14:25:55 -0300

    Adds horizontal scrolling

--- cuvim.c
+++ cuvim.c
@@ -42,6 +42,7 @@ struct editorConfig
 {
   int cx, cy;
   int rowoff;
+  int coloff;
   int screenrows;
   int screencols;
   int numrows;
@@ -81,6 +82,7 @@ void initEditor()
     E.cx = 0;
     E.cy = 0;
     E.rowoff = 0;
+    E.coloff = 0;
     E.numrows = 0;
     E.row = NULL;
 
@@ -292,6 +294,12 @@ void editorScroll()
   {
     E.rowoff = E.cy - E.screenrows + 1;
   }
+  if (E.cx < E.coloff) {
+    E.coloff = E.cx;
+  }
+  if (E.cx >= E.coloff + E.screencols) {
+    E.coloff = E.cx + E.screencols + 1;
+  }
 }
 
 void editorRefreshScreen()
@@ -306,7 +314,7 @@ void editorRefreshScreen()
   editorDrawRows(&ab);
 
   char buf[32];
-  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx - E.coloff + 1);
   abAppend(&ab, buf, strlen(buf));
 
   abAppend(&ab, "\x1b[?25h", 6);
@@ -341,9 +349,10 @@ void editorDrawRows(struct abuf *ab)
         abAppend(ab, "~", 1);
       }
     } else {
-      int len = E.row[filerow].size;
+      int len = E.row[filerow].size - E.coloff;
+      if (len < 0) len = 0;
       if (len > E.screencols) len = E.screencols;
-      abAppend(ab, E.row[filerow].chars, len);
+      abAppend(ab, &E.row[filerow].chars[E.coloff], len);
     }
 
     abAppend(ab, "\x1b[K", 3);
@@ -416,10 +425,7 @@ void editorMotion(int key)
         }
         break;
     case ARROW_RIGHT:
-        if (E.cx != E.screenrows - 1)
-        {
-          E.cx++;
-        }
+        E.cx++;
         break;
   }
 }
